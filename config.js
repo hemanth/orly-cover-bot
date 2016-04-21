aecdfac6c489f2ea86dab975ac8ccb953bfc8aa2
Hemanth.HM <hemanth.hm@gmail.com>
2016-04-21 22:52:00 +0530

    Dummy config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token_key:     '',
+    access_token_secret:  '',
+    timeout_ms:           60 * 1000,  // optional HTTP request timeout to apply to all requests.
+};
